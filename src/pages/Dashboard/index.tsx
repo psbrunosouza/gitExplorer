7110ffb7185b8ca28a5db1e630b190c63e0d15ae
psbrunosouza <psbrunosouza@gmail.com>
2021-01-17 16:36:23 -0300

    working with styled components and polished libs

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,64 @@
 import React from 'react';
-import { Title } from './dashboard-style';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './dashboard-style';
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="git-explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="digite o nome do repositório" type="text" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/45085894?s=460&u=49bb5eee4513e3bae1a3bc55bf6df57f9cc35e56&v=4"
+            alt="Bruno Pinheiro"
+          />
+          <div>
+            <strong>Bruno teste repositorio</strong>
+            <p>
+              repositório de testes, para um site que está igualmente testando
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/45085894?s=460&u=49bb5eee4513e3bae1a3bc55bf6df57f9cc35e56&v=4"
+            alt="Bruno Pinheiro"
+          />
+          <div>
+            <strong>Bruno teste repositorio</strong>
+            <p>
+              repositório de testes, para um site que está igualmente testando
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/45085894?s=460&u=49bb5eee4513e3bae1a3bc55bf6df57f9cc35e56&v=4"
+            alt="Bruno Pinheiro"
+          />
+          <div>
+            <strong>Bruno teste repositorio</strong>
+            <p>
+              repositório de testes, para um site que está igualmente testando
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
